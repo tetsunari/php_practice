f910c99516deccbbc95886e8163f0ba21ac75f41
Tetsu Matsu <mtst.tty556@gmail.com>
2021-01-25 23:43:19 +0900

    constract

--- practice6.php
+++ practice6.php
@@ -13,27 +13,45 @@
 // show($posts[0]);
 // show($posts[1]);
 
+// //クラス
+// class Post
+// {
+//   //プロパティ 
+//   public $text;
+//   public $likes;
+//   //メソッド
+//   public function show()
+//   {
+//     printf('%s (%d)' .PHP_EOL, $this->text, $this->likes);
+//   }
+// }
+// $posts = [];
+// $posts[0] = new Post();
+// $posts[0]->text = 'hello';
+// $posts[0]->likes = 0;
+// $posts[1] = new Post();
+// $posts[1]->text = 'hello world';
+// $posts[1]->likes = 1;
+// $posts[0]->show();
+// $posts[1]->show();
 
-//クラス
+//コンストラクタ
 class Post
 {
-  //プロパティ 
   public $text;
   public $likes;
-
-  //メソッド
+  public function __construct($text, $likes)
+  {
+    $this->text = $text;
+    $this->likes = $likes;
+  }
   public function show()
   {
     printf('%s (%d)' .PHP_EOL, $this->text, $this->likes);
   }
 }
 $posts = [];
-$posts[0] = new Post();
-$posts[0]->text = 'hello';
-$posts[0]->likes = 0;
-$posts[1] = new Post();
-$posts[1]->text = 'hello world';
-$posts[1]->likes = 1;
+$posts[0] = new Post('hello', 0);
+$posts[1] = new Post('hello world', 1);
 $posts[0]->show();
 $posts[1]->show();
-
