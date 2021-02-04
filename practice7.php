f816bcc6cb93f704540f057cc1b5501cc876d288
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-04 15:50:33 +0900

    interface

--- practice7.php
+++ practice7.php
@@ -42,6 +42,66 @@
 // }
 
 //さらにクラス追加
+// abstract class BasePost
+// {
+//   protected $text;
+//   public function __construct($text)
+//   {
+//     $this->text = $text;
+//   }
+//   abstract public function show();
+// }
+// class Post extends BasePost
+// {
+//   public function show()
+//   {
+//     printf('%s' . PHP_EOL, $this->text);
+//   }
+// }
+// class SponsoredPost extends BasePost
+// {
+//   private $sponsor;
+//   public function __construct($text, $sponsor)
+//   {
+//     parent::__construct($text);
+//     $this->sponsor = $sponsor;
+//   }
+//   public function show()
+//   {
+//       printf('%s by %s' . PHP_EOL, $this->text, $this->sponsor);
+//   }
+// }
+// class PremiumPost extends BasePost
+// {
+//   private $price;
+//   public function __construct($text, $price)
+//   {
+//     parent::__construct($text);
+//     $this->price = $price;
+//   }
+//   public function show()
+//   {
+//       printf('%s [%d JPY]' . PHP_EOL, $this->text, $this->price);
+//   }
+// }
+// $posts = [];
+// $posts[0] = new Post('hello');
+// $posts[1] = new Post('hello again');
+// $posts[2] = new SponsoredPost('hello hello', 'dotinstall');
+// $posts[3] = new PremiumPost('hello there', '3000');
+// function processPost(BasePost $post) 
+// {
+//   $post->show();
+// }
+// foreach ($posts as $post) {
+//   processPost($post);
+// }
+
+//イターフェイスの実装  クラスは複数の親クラスを持てない
+interface LikeInterface
+{
+  public function like();
+}
 abstract class BasePost
 {
   protected $text;
@@ -51,11 +111,16 @@ abstract class BasePost
   }
   abstract public function show();
 }
-class Post extends BasePost
+class Post extends BasePost implements LikeInterface
 {
+  private $likes = 0;
+  public function like()
+  {
+    $this->likes++;
+  }
   public function show()
   {
-    printf('%s' . PHP_EOL, $this->text);
+    printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
   }
 }
 class SponsoredPost extends BasePost
@@ -71,8 +136,13 @@ class SponsoredPost extends BasePost
       printf('%s by %s' . PHP_EOL, $this->text, $this->sponsor);
   }
 }
-class PremiumPost extends BasePost
+class PremiumPost extends BasePost implements LikeInterface
 {
+  private $likes = 0;
+  public function like()
+  {
+    $this->likes++;
+  }
   private $price;
   public function __construct($text, $price)
   {
@@ -81,7 +151,7 @@ class PremiumPost extends BasePost
   }
   public function show()
   {
-      printf('%s [%d JPY]' . PHP_EOL, $this->text, $this->price);
+      printf('%s (%d) [%d JPY]' . PHP_EOL, $this->text, $this->likes, $this->price);
   }
 }
 $posts = [];
@@ -89,6 +159,8 @@ $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
 $posts[2] = new SponsoredPost('hello hello', 'dotinstall');
 $posts[3] = new PremiumPost('hello there', '3000');
+$posts[0]->like();
+$posts[3]->like();
 function processPost(BasePost $post) 
 {
   $post->show();
